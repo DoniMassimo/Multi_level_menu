44284b51e157549761182103b3c2fb005fc1888b
DoniMassimo <donimassimo3@gmail.com>
2022-01-05 12:46:39 +0100

    Update multi_selectable_menu.py

--- multi_selectable_menu.py
+++ multi_selectable_menu.py
@@ -5,7 +5,7 @@
 import support_library_v1 as sl
 from curtsies import Input
 import sys
-
+#commento
 def get_list_index(list, index):
     if index < len(list) and index >= 0:
         return list[index]
